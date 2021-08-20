8d8c41f6296256e2ce564d28d59a715f67a69f49
BrownLechuti <brownlechuti@gmail.com>
2021-08-20 18:59:49 +0200

    First project commit

---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/dto/newName.java
@@ -0,0 +1,6 @@
+package za.ac.nwu.ac.domain.dto;
+
+public class newName
+{
+
+}
---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/exception/Demo.java
@@ -0,0 +1,8 @@
+package za.ac.nwu.ac.domain.exception;
+
+import za.ac.nwu.ac.domain.dto.newName;
+
+public class Demo
+{
+    private newName newName;
+}
