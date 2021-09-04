87416f18bd45498257f16c0216483ba147791560
BrownLechuti <brownlechuti@gmail.com>
2021-09-04 17:02:19 +0200

    run application

--- AccountSystemSpringBoot/src/main/java/za/ac/nwu/web/sb/controller/Controller.java
+++ AccountSystemSpringBoot/src/main/java/za/ac/nwu/web/sb/controller/Controller.java
@@ -1,16 +1,17 @@
 package za.ac.nwu.web.sb.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+/*import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import za.ac.nwu.ac.domain.service.GeneralResponse;*/
 
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-/*import za.ac.nwu.ac.domain.service.GeneralResponse;*/
+
 
 @RestController
 public class Controller
 {
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHelloWorld()
     {
         return "Hello World";
---
+++ AccountSystemSpringBoot/src/main/java/za/ac/nwu/web/sb/exception/ex.java
@@ -0,0 +1,5 @@
+package za.ac.nwu.web.sb.exception;
+
+public class ex
+{
+}
