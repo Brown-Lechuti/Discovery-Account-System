440507a49ae6d6bb621882862d118885cc39b310
BrownLechuti <brownlechuti@gmail.com>
2021-09-04 22:15:53 +0200

    application running still no output

--- AccountSystemSpringBoot/src/main/java/za/ac/nwu/web/sb/Config/WebConfig.java
+++ AccountSystemSpringBoot/src/main/java/za/ac/nwu/web/sb/Config/WebConfig.java
@@ -2,7 +2,10 @@ package za.ac.nwu.web.sb.Config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+/*import org.springframework.web.servlet.config.annotation.EnableWebMvc;*/
+
 @Configuration
+/*@EnableWebMvc*/
 @ComponentScan(basePackages = {
         "za.ac.nwu.ac.web.sb.controller",
         "za.ac.nwu.ac.web.sb.exception"
--- AccountSystemSpringBoot/src/main/java/za/ac/nwu/web/sb/controller/Controller.java
+++ AccountSystemSpringBoot/src/main/java/za/ac/nwu/web/sb/controller/Controller.java
@@ -2,20 +2,34 @@ package za.ac.nwu.web.sb.controller;
 
 /*import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import za.ac.nwu.ac.domain.service.GeneralResponse;*/
-
+import za.ac.nwu.ac.domain.service.GeneralResponse;*
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;*/
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import za.ac.nwu.web.sb.service.Greeting;
 
 @RestController
 public class Controller
 {
-    @RequestMapping("/")
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name)
+    {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+
+    /*@RequestMapping("/")
     public String getHelloWorld()
     {
         return "Hello World";
-    }
+    }*/
    /* @GetMapping("/ping")
     public GeneralResponse<String> ping(@ResponseParam(value = "echo", defaultvalue = "pong") String echo)
     {
