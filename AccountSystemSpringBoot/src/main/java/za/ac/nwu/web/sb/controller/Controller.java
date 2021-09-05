f0d992dee76cb3fc247f3323d86da12a893ea82a
BrownLechuti <brownlechuti@gmail.com>
2021-09-05 15:52:23 +0200

    application RUNNING SUCCESS 2

--- AccountSystemSpringBoot/src/main/java/za/ac/nwu/web/sb/controller/Controller.java
+++ AccountSystemSpringBoot/src/main/java/za/ac/nwu/web/sb/controller/Controller.java
@@ -15,24 +15,13 @@ import za.ac.nwu.web.sb.service.Greeting;
 @RestController
 public class Controller
 {
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name)
-    {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
-
-    /*@RequestMapping("/")
-    public String getHelloWorld()
-    {
-        return "Hello World";
-    }*/
-   /* @GetMapping("/ping")
-    public GeneralResponse<String> ping(@ResponseParam(value = "echo", defaultvalue = "pong") String echo)
-    {
-        return new GeneralResponse<String>(successful: true, echo);
-    }*/
 }
