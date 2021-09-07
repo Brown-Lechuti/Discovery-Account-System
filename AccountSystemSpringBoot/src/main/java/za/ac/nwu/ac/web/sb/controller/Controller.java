3115cb0fd76c0a78410cb07a351f9d6d0c009a04
BrownLechuti <brownlechuti@gmail.com>
2021-09-07 19:38:29 +0200

    SWAGGER APP WORKING

--- AccountSystemSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/Controller.java
+++ AccountSystemSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/Controller.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import za.ac.nwu.ac.domain.service.GeneralResponse;*
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;*/
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import za.ac.nwu.ac.web.sb.service.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class Controller
