c4d4bedb576eeef65c44c22a0dca777815504707
BrownLechuti <brownlechuti@gmail.com>
2021-09-07 19:58:28 +0200

    SWAGGER APP WORKING STABLE

--- AccountSystemSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/Controller.java
+++ AccountSystemSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;*/
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import za.ac.nwu.ac.web.sb.service.Greeting;
@@ -16,13 +17,16 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class Controller
 {
-
-    private static final String template = "Hello, %s!";
+    @RequestMapping(value = "/hello")
+    public String Hello() {
+        return "Hello World";
+    }
+    /*private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
+    }*/
 
 }
