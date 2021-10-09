ef3682bb6e602f3399d958026396bcc9debf3300
BrownLechuti <brownlechuti@gmail.com>
2021-10-10 00:50:45 +0200

    Added some code coverage with jacoco

---
+++ AccountSystemSpringBoot/src/test/java/DetailMemberServiceTest.java
@@ -0,0 +1,54 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import za.ac.nwu.ac.domain.dto.Member;
+import za.ac.nwu.ac.repo.dto.Member_Repo;
+import za.ac.nwu.ac.web.sb.exceptions.ResourceNotFoundException;
+import za.ac.nwu.ac.web.sb.services.DetailMemberService;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DetailMemberServiceTest {
+
+    @Mock
+    private Member_Repo repo;
+
+    @InjectMocks
+    private DetailMemberService detailUserService;
+
+    @Test
+    public void whenGivenId_shouldReturnUser_ifFound() throws ResourceNotFoundException {
+        Member user = new Member();
+        user.setId(89L);
+
+        when(repo.findById(user.getId())).thenReturn(Optional.of(user));
+
+        Member expected = detailUserService.listMember(user.getId());
+
+        assertThat(expected).isSameAs(user);
+        verify(repo).findById(user.getId());
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void should_throw_exception_when_user_doesnt_exist() throws ResourceNotFoundException {
+        Member user = new Member();
+        user.setId(89L);
+        user.setFirst_name("Name");
+        user.setLast_name("Surname");
+        user.setActivation_date("2021-10-02");
+        user.setCurrency(150);
+        user.setPlays(5);
+
+        given(repo.findById(anyLong())).willReturn(Optional.ofNullable(null));
+        detailUserService.listMember(user.getId());
+    }
+}
---
+++ AccountSystemSpringBoot/src/test/java/ViewMilesTest.java
@@ -0,0 +1,66 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import za.ac.nwu.ac.domain.dto.Member;
+import za.ac.nwu.ac.web.sb.controllers.ViewMiles;
+import za.ac.nwu.ac.web.sb.exceptions.ResourceNotFoundException;
+import za.ac.nwu.ac.web.sb.services.DetailMemberService;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ViewMiles.class)
+public class ViewMilesTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private DetailMemberService detailUserService;
+
+    @Test
+    public void listUserById_whenGetMethod() throws Exception {
+
+        Member user = new Member();
+        user.setFirst_name("Name");
+        user.setLast_name("Surname");
+        user.setActivation_date("2021-10-02");
+        user.setCurrency(150);
+        user.setPlays(5);
+        user.setId(89L);
+
+        given(detailUserService.listMember(user.getId())).willReturn(user);
+
+        mvc.perform(get("/MEMBER_/" + user.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.first_name", is(user.getFirst_name())));
+    }
+
+    @Test
+    public void should_throw_exception_when_user_doesnt_exist() throws Exception {
+        Member user = new Member();
+        user.setId(89L);
+        user.setFirst_name("Name");
+        user.setLast_name("Surname");
+        user.setActivation_date("2021-10-02");
+        user.setCurrency(150);
+        user.setPlays(5);
+
+        Mockito.doThrow(new ResourceNotFoundException("Member account not found for this id :: "+user.getId())).when(detailUserService).listMember(user.getId());
+
+        mvc.perform(get("/MEMBER_/" + user.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+}
